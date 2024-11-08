0c48a7427f8f079115f526ae0781d6e2e1c58b9e
pretao <pretao@pretao>
2024-11-08 00:42:39 -0300

    button downloadCv working

--- src/script.js
+++ src/script.js
@@ -7,3 +7,11 @@ document.querySelectorAll('.card').forEach(card => {
         card.style.transform = 'scale(1)'; // Retorna ao tamanho original ao retirar o mouse
     });
 });
+
+
+function baixarCurriculo() {
+    const link = document.createElement('a');  // Cria um elemento <a> dinamicamente
+    link.href = 'assets/curriculo novo.pdf';  // Caminho do arquivo que será baixado
+    link.download = 'Curriculo-Samuel';  // Nome do arquivo ao ser baixado
+    link.click();  // Simula o clique no link para iniciar o download
+}
